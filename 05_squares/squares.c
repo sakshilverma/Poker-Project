20696e8987ac21141b12bbb719ced235900b62c5
Coursera Learner <nobody@nowhere.nul>
2020-05-11 08:57:52 +0000

    Did assignment 05

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -9,7 +9,7 @@
   
     int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
-      if(coord>=offset && coord < (offset + size))
+      if(coord>=offset && coord<(offset+size))
 	return 1;
        // else, return 0
       else return 0;
@@ -21,8 +21,7 @@
  */
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
-  
-  if(coord==offset || coord==offset+size)
+  if(coord==offset || coord==(offset+size))
     return 1;
   // return 1, else return 0
   else
@@ -52,19 +51,33 @@ int h=max(size1, y_offset+size2);
       //  OR
       //    ((y is between y_offset and y_offset + size2) AND
       //x is equal to either x_offset OR x_offset + size2 -1)
-       if(((x>=x_offset && x<x_offset+size2) && (y=y_offset || y==y_offset+size2-1)) || ((y>=y_offset && y<y_offset+size2) && (x=x_offset || x==x_offset+size2-1)))
+       
+	 if(x>=x_offset && x<x_offset+size2)
+	 {if(y==y_offset || y==y_offset+size2-1)
+	     printf("*");
+	   continue;}
+       else if(y>=y_offset && y<y_offset+size2)
+	 {if(x==x_offset || x==x_offset+size2-1)
 	 // if so, print a *
-	 printf("*");
-      //if not,
+	     printf("*");
+	  continue;}
+          //if not,
          // check if EITHER
  //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
 //    y is less than size1 AND (x is either 0 or size1-1)
-       if((x<size1 && (y=0 || y==size1-1)) || (y<size1 && (x=0 || x==size1-1)))
+       
+       else if(x<size1)
+	 {if(y==0 || y==size1-1)
+	     printf("#");
+	   continue;	   }
+	  else if(y<size1)
+	    {if(x==0 || x==size1-1)
 	 //if so, print a #
-	 printf("#");
+		printf("#");
+	      continue;}
       //else print a space
-       else printf(" ");
+	      else printf(" ");
        //when you finish counting x from 0 to w,
      }
     //print a newline
