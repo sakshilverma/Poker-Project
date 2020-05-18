00e88a381544fedb50d2605f096989cf07fc80fa
Coursera Learner <nobody@nowhere.nul>
2020-05-18 15:21:54 +0000

    Did assignment 07

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -5,18 +5,18 @@ struct _retire_info {
   int months;
   double contribution;
   double rate_of_return;
-
 };
 typedef struct _retire_info retire_info;
 
 double account_balance (int startAge, double initial, retire_info x)
-{ for(int i=startAge;i<=x.months;i++)
-    { printf("Age %3d month %2d you have $%.2lf\n", i/12, i%12, initial);
+{ for(int i=startAge; i<x.months+startAge; i++)
+    {printf("Age %3d month %2d you have $%.2lf\n", i/12, i%12, initial);
       initial= (initial*x.rate_of_return) + initial;
       initial= initial+x.contribution;
     }
   return initial;
 }
+
 
 void retirement (int startAge, double initial, retire_info working, retire_info retired)
 { double y=account_balance (startAge, initial, working);
@@ -31,11 +31,11 @@ int main()
   retire_info retired;
 
   working.months=489;
-  working.contribution=10000;
+  working.contribution=1000;
   working.rate_of_return=0.045/12;
 
   retired.months=384;
-  retired.contribution=-40000;
+  retired.contribution=-4000;
   retired.rate_of_return=0.01/12;
 
   retirement (327, 21345, working, retired);
