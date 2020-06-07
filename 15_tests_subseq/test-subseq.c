668f322d01f29e14dbaa1ae3369dd118682debc1
Coursera Learner <nobody@nowhere.nul>
2020-06-07 10:44:35 +0000

    Did assignment

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -5,36 +5,63 @@ size_t maxSeq(int * array, size_t n);
 
 int main()
 {
-  
-    int a[]={3,4,-3};
-  if (maxSeq(a,3) != 2)
+  int a[]={1,3,5,4,9};
+  if (maxSeq(a,5) != 3)
     {
     return EXIT_FAILURE;
   }
 
-  int b[]={1,6,8,7,90,102,150,256};
-  if (maxSeq(b,8) != 5)
+  int b[]={1,5,3,6,4,7,2,9,8};
+  if (maxSeq(b,9) != 2)
     {
-    return EXIT_FAILURE;
-  }
-
-  int c[]={-90,-84,-65,4,-123};
-  if (maxSeq(c,5) != 4)
-    {
-    return EXIT_FAILURE;
+      return EXIT_FAILURE;
     }
 
-  int d[]={-34,-31,-12,-56,3,4,5};
-  if (maxSeq(d,7) != 3)
+  int c[]={1};
+  if (maxSeq(c,1) != 1)
     {
-    return EXIT_FAILURE;
-  }
-
-  int e[]={1,2,1,3,5,7,2,4,6,9};
-  if (maxSeq(e,10) != 4)
-    {
-    return EXIT_FAILURE;
-  }
+      return EXIT_FAILURE;
+    }
   
-return EXIT_SUCCESS;
+  int d[]={-5,-4,-3,-2,-1,0};
+  if (maxSeq(d,6) != 6)
+    {
+      return EXIT_FAILURE;
+    }
+
+  int f[]={};
+  if (maxSeq(f,0) != 0)
+    {
+      return EXIT_FAILURE;
+    }
+
+  int g[]={-1000,-11,-2,-2000};
+  if (maxSeq(g,4) != 3)
+    {
+      return EXIT_FAILURE;
+    }
+
+  int h[]={2,1000};
+  if (maxSeq(h,2) != 2)
+    {
+      return EXIT_FAILURE;
+    }
+  int i[]={0,50,1000};
+  if (maxSeq(i,3) != 3)
+    {
+      return EXIT_FAILURE;
+    }
+  int j[]={2,4,6,6};
+  if (maxSeq(j,4) != 3)
+    {
+      return EXIT_FAILURE;
+    }
+
+  int k[]={1,2,7,3,8,12,16};
+  if (maxSeq(k,7) != 4)
+    {
+      return EXIT_FAILURE;
+    }
+  
+  return EXIT_SUCCESS;
 }
