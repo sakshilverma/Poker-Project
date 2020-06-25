c2c510dad92d11aee1bc2567319ef9314f2ae90c
Coursera Learner <nobody@nowhere.nul>
2020-06-25 07:50:14 +0000

    Did assignment 23

--- 23_power_rec/test-power.c
+++ 23_power_rec/test-power.c
@@ -1,1 +1,22 @@
-../22_tests_power/test-power.c
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+unsigned power (unsigned x, unsigned y);
+
+int main()
+{
+  if(power(0,0) != 1)
+    {exit(EXIT_FAILURE);
+    }
+
+  if(power(25,2) != 625)
+    {exit(EXIT_FAILURE);
+    }
+
+  if(power(121345,1) != 121345)
+    {exit(EXIT_FAILURE);
+    }
+
+  else exit(EXIT_SUCCESS);
+}
