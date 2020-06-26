cc248b9050f4f36ba2fced22f3222a00d87daa6b
Coursera Learner <nobody@nowhere.nul>
2020-06-26 14:50:29 +0000

    Did assignment deck

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -21,7 +21,7 @@ int deck_contains(deck_t * d, card_t c) {
 
 void shuffle(deck_t * d){
   card_t temp;
-  for(size_t i=0;i<(d->n_cards)/2;i++)
+  for(int i=0;i<(d->n_cards)/4;i++)
     {
 int y = random()%d->n_cards;
  temp=*(d->cards[i]);
