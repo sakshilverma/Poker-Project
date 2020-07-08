5315530247b7a4ca5f26bacfff31741a47ca6dd5
Coursera Learner <nobody@nowhere.nul>
2020-07-08 11:47:56 +0000

    Did assignment 25

--- 25_break_encr/ans.c
+++ 25_break_encr/ans.c
@@ -5,7 +5,7 @@
 int freq(FILE *f)
 { int a[26]={0};
   int b,k,n=0;
-  while((b=fgetc(f)) != EOF && n<=100)
+  while((b=fgetc(f)) != EOF && n<=500)
     {if(isalpha(b))
 	{b=tolower(b); 
 	  k=b-'a';
