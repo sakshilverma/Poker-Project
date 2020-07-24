91823d5ae731bc2357e56da3a378ac16a51c875b
Coursera Learner <nobody@nowhere.nul>
2020-07-24 11:16:02 +0000

    Did assignment 30

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -17,8 +17,46 @@ void sortData(char ** data, size_t count) {
 }
 
 int main(int argc, char ** argv) {
-  
-  //WRITE YOUR CODE HERE!
-  
+    //WRITE YOUR CODE HERE!
+  if(argc>=0)
+    {
+      if(argc==1)
+	{ FILE *f=fopen(argv[0], "r");
+	  char *line=0;
+  size_t sz=0;
+  while(getline(&line, &sz, f) >= 0)
+    {char ** data=&line;
+      size_t count=sz;
+      sortData(data, count);
+      printf("%s", line);
+      }
+  free(line);
+  if(fclose(f) != 0)
+    {perror("Failed to close the input file\n");
+      return EXIT_FAILURE;
+    }}
+
+      if(argc>1)
+	{for(int i=0;i<argc;i++)
+	{ FILE *f=fopen(argv[i], "r");
+	  char *line=0;
+  size_t sz=0;
+  while(getline(&line, &sz, f) >= 0)
+    {char ** data=&line;
+      size_t count=sz;
+      sortData(data, count);
+      printf("%s", line);
+      }
+  free(line);
+  if(fclose(f) != 0)
+    {perror("Failed to close the input file\n");
+      return EXIT_FAILURE;
+    	}
+	}}
+    }
+  if(argc<0){
+  return EXIT_FAILURE;
+  }
+
   return EXIT_SUCCESS;
 }
