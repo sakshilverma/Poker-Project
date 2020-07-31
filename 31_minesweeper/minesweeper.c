de2e9b2b0cf5f4a6deb2bc4994ef3f4b01c7d96e
Coursera Learner <nobody@nowhere.nul>
2020-07-31 05:31:23 +0000

    Did assignment 31

--- 31_minesweeper/minesweeper.c
+++ 31_minesweeper/minesweeper.c
@@ -119,7 +119,7 @@ int countMines(board_t * b, int x, int y) {
   int count=0;
   for(int j=y-1;j<=y+1;j++)
     {for(int i=x-1;i<=x+1;i++)
-	{if(i==x && j==y)
+	{if(i!=x && j!=y)
 	  {if(i >= 0 && j >= 0 && i < b->width && y < b->height)
 	    {	  if(IS_MINE(b->board[j][i]))
 	  {count++;
