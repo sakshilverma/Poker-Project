b54317fb5c8b9ae6b092d068b376fc875616f761
Coursera Learner <nobody@nowhere.nul>
2020-09-04 17:51:01 +0000

    Did assignment 32

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -7,7 +7,7 @@ char *splitLine(char *line){
   char *value=strchr(line,'=');
   *value='\0';
   value++;
-  value[strlen(value) -1]='\0';
+  value[strlen(value) - 1]='\0';
   return value;
 }
 
@@ -17,25 +17,26 @@ kvarray_t * readKVs(const char * fname) {
   if(f==NULL){
     perror("Invalid file\n");
   }
-  kvarray_t *kvarray=malloc(sizeof(*kvarray));
-  size_t numPairs=0;
+  kvarray_t *kvarray=NULL;
+ kvarray=(kvarray_t*)malloc(100 * sizeof(kvarray_t));
+ kvarray->pair=malloc(100 * sizeof(*kvarray->pair));
+   
+ size_t numPairs=0;
   char *line=NULL;
   size_t sz;
   while(getline(&line,&sz,f) != -1){
     if(line[0]=='\n'){
       continue;
     }
-    kvarray->pair=realloc(kvarray->pair, ((numPairs+1) * sizeof(*kvarray->pair)));
     kvarray->pair[numPairs]=malloc(sizeof(*kvarray->pair[numPairs]));
-    kvarray->pair[numPairs]->value=malloc(sizeof(*kvarray->pair[numPairs]->value));
-    kvarray->pair[numPairs]->value=splitLine(line);
-    kvarray->pair[numPairs]->key=malloc(sizeof(*kvarray->pair[numPairs]->key));    
-    kvarray->pair[numPairs]->key=line;
-    numPairs++;
+ kvarray->pair[numPairs]->value=splitLine(line);
+   kvarray->pair[numPairs]->key=line;
+   numPairs++;
     line=NULL;
   }
+  
   free(line);
-  kvarray->numPairs=numPairs;
+kvarray->numPairs=numPairs;
   fclose(f);
   return kvarray;
 }
@@ -44,9 +45,7 @@ void freeKVs(kvarray_t * pairs) {
   //WRITE ME
   for(int i=0;i<pairs->numPairs;i++){
     free(pairs->pair[i]->key);
-    free(pairs->pair[i]->value);
-    free(pairs->pair[i]);
-    
+    free(pairs->pair[i]);    
   }
   free(pairs->pair);
   free(pairs);
