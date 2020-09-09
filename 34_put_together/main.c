26a7b94cf12e62535ec25b0cd8bc49446d691983
Coursera Learner <nobody@nowhere.nul>
2020-09-09 15:20:08 +0000

    Did assignment 34

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -36,7 +36,7 @@ return NULL;
 
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
-  if(argc<3){
+  if(argc<2){
     return EXIT_FAILURE;
   }
   //read the key/value pairs from the file named by argv[1] (call the result kv)
@@ -45,7 +45,7 @@ int main(int argc, char ** argv) {
     return EXIT_FAILURE;
   }
   //count from 2 to argc (call the number you count i)
-  for(int i=2;i<argc;i++){
+  for(int i=2;i<=argc;i++){
     //count the values that appear in the file named by argv[i], using kv as the key/value pair
     //   (call this result c)
     counts_t * c=countFile(argv[i], kv); 
