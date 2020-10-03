705c1b2219024a2e903856791013392ba73ff8f8
Coursera Learner <nobody@nowhere.nul>
2020-10-03 20:31:58 +0000

    Did assignment c4prj2_input

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -5,7 +5,7 @@
 #include "deck.h"
 
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
- if(index+1 > fc->n_decks){
+ if(index+1 >= fc->n_decks){
 	fc->decks=realloc(fc->decks,(index + 1)*sizeof(*fc->decks));
 	for (size_t i=fc->n_decks;i<index+1;i++){
 	    fc->decks[i].cards=NULL;
@@ -13,10 +13,12 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
 	  }
 	fc->n_decks=index+1;
       }
- fc->decks[index].cards=realloc(fc->decks[index].cards,(fc->decks[index].n_cards+1)*sizeof(*fc->decks[index].cards));
+ else {
+   fc->decks[index].cards=realloc(fc->decks[index].cards,(fc->decks[index].n_cards+1)*sizeof(*fc->decks[index].cards));
     fc->decks[index].cards[fc->decks[index].n_cards]=ptr;
     fc->decks[index].n_cards++;
-  }
+ }
+}
 
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
   if(deck->n_cards < fc->n_decks){
